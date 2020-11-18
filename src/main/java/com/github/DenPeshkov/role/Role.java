2795fafbf1c70a702508084a021c71a1653bfe9e
Peshkov Denis <peshkovdenis1985@gmail.com>
2020-11-18 22:24:23 +0300

    added DAO objects

--- src/main/java/com/github/DenPeshkov/role/Role.java
+++ src/main/java/com/github/DenPeshkov/role/Role.java
@@ -1,9 +1,11 @@
 package com.github.DenPeshkov.role;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.github.DenPeshkov.user.User;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 @Entity
 public class Role {
@@ -12,6 +14,9 @@ public class Role {
   private Long id;
 
   private String name;
+
+  @ManyToMany(mappedBy = "roles")
+  private Set<User> users = new HashSet<>();
 
   public Role() {}
 
@@ -27,12 +32,33 @@ public class Role {
     return name;
   }
 
+  public Set<User> getUsers() {
+    return users;
+  }
+
   public void setId(Long id) {
     this.id = id;
   }
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public void setUsers(Set<User> users) {
+    this.users = users;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Role role = (Role) o;
+    return getId().equals(role.getId());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getId());
   }
 
   @Override
--- src/main/java/com/github/DenPeshkov/user/User.java
+++ src/main/java/com/github/DenPeshkov/user/User.java
@@ -1,7 +1,11 @@
 package com.github.DenPeshkov.user;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import com.github.DenPeshkov.role.Role;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 @Entity
 public class User {
@@ -9,6 +13,13 @@ public class User {
 
   private String name;
   private String password;
+
+  @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+  @JoinTable(
+      name = "user_role",
+      joinColumns = @JoinColumn(name = "user_id"),
+      inverseJoinColumns = @JoinColumn(name = "role_id"))
+  private Set<Role> roles = new HashSet<>();
 
   public User() {}
 
@@ -30,6 +41,10 @@ public class User {
     return password;
   }
 
+  public Set<Role> getRoles() {
+    return roles;
+  }
+
   public void setLogin(String login) {
     this.login = login;
   }
@@ -40,6 +55,33 @@ public class User {
 
   public void setPassword(String password) {
     this.password = password;
+  }
+
+  public void setRoles(Set<Role> roles) {
+    this.roles = roles;
+  }
+
+  public void addRole(Role role) {
+    roles.add(role);
+    role.getUsers().add(this);
+  }
+
+  public void removeRole(Role role) {
+    roles.remove(role);
+    role.getUsers().remove(this);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    User user = (User) o;
+    return getLogin().equals(user.getLogin());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getLogin());
   }
 
   @Override
