cc5f67fc02a1aaccfe21a37800da642e5034ef40
Peshkov Denis <peshkovdenis1985@gmail.com>
2020-11-20 01:56:11 +0300

    refactored RoleRepository

--- src/main/java/com/github/DenPeshkov/role/RoleRepository.java
+++ src/main/java/com/github/DenPeshkov/role/RoleRepository.java
@@ -1,5 +1,11 @@
 package com.github.DenPeshkov.role;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface RoleRepository extends CrudRepository<Role, Long> {}
+import java.util.Set;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+  Role findByName(String name);
+}
